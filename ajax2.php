0cdbb29b984efcc86f0bc0ea48e5dac799864865
Andrii Hutnyk <andreigutnik@gmail.com>
2023-07-14 18:01:00 +0200

    Update ajax2.php

--- ajax2.php
+++ ajax2.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST["ter"])) $ter = $_POST["ter"];
 
-if(!mysql_connect(localhost, "andrei", "203523")) exit;
+if(!mysql_connect(localhost, "andrei", "******")) exit;
 mysql_select_db("temp");
 $query = mysql_query("SELECT * FROM tp_menu WHERE id=2");
 $res = mysql_fetch_assoc($query);
